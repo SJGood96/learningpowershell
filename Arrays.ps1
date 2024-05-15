f5fecaa25e4048a5ecfe191c4510deba29b74d38
Spencer Good <56788071+SJGood96@users.noreply.github.com>
2024-05-15 14:58:14 -0400

    Add files via upload

---
+++ Arrays.ps1
@@ -0,0 +1,15 @@
+### Arrays
+
+# Assign multiple values to a variable
+$a=2, 42, 5, 7, 13
+
+# Can assign witha range operator
+$a=1..10
+
+#By default, array data type is object
+$a.GetType()
+
+# Because of this, you can store multiple data types in an array
+$a = 24, 'ITProTV'
+
+# Create array using the sub-expression operator
